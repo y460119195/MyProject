ab190ca4b0dcae0a8edcef0dcb184a2ac23ab615
张贇彬 <zhangyunbin@zhangyunbindeMacBook-Pro.local>
2019-07-22 22:41:51 +0800

    chunk

--- lodash/y460119195-lodash.js
+++ lodash/y460119195-lodash.js
@@ -2,4 +2,14 @@ var y460119195 = {
   compact: function(ary) {
     return ary.filter(it => it)
   },
+  chunk: function(ary,size = 1) {
+    var result = []
+    for(var i in ary){
+      var subscript = (Math.floor(i+1)/size)-1
+      if(!result[subscript])
+      result[subscript] = []
+      result[subscript].push(ary[i])
+    }
+    return result
+  },
 }
